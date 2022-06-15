c9564ee18e63e5c0d62567b35e01c77a08e179cb
isaacbrds <isaac.brigido@gmail.com>
2022-06-15 09:28:50 -0300

    Aula 01

---
+++ python/aula01.py
@@ -0,0 +1,5 @@
+print('Hello World!') 
+print('Seja bem vindo ao curso de Python do Coding Tank!')
+
+linguagem = input("Qual é a linguagem de programação que você está estudando?\n")
+print("Massa você está estudando ", linguagem)
