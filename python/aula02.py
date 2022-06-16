c79deba6acba22c9a1186c83b6088838fbb839dc
isaacbrds <isaac.brigido@gmail.com>
2022-06-16 11:20:00 -0300

    Aula 02 e Aula 03

---
+++ python/aula02.py
@@ -0,0 +1,9 @@
+#encoding: utf-8
+# Variáveis
+
+idade  = 30
+altura = 1.81
+nome = 'Isaac'
+estudando = True
+
+print(idade, altura, nome, estudando)
---
+++ python/aula03.py
@@ -0,0 +1,10 @@
+numero1 = 10
+numero2 = 20
+
+print(numero1 + numero2)
+print(numero1 - numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+print(numero1 // numero2)
+print(numero1 % numero2)
+print(numero1 ** numero2)
